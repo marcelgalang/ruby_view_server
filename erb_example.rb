8ed67086234a114a093b9e353304ade16fb15cf1
marcelgalang <marcelgalang@gmail.com>
2015-04-20 11:20:18 -0400

    homework section 1 completed

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,17 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+beatles = ['john', 'paul', 'george','ringo']
+beatles.each do |beatle|
+	puts "#{beatle} is the best"
+end
+
+names = Hash.new
+names[1] = "Mick"
+names[2] = "Keef"
+
+puts names
+
+puts "went to the dance"
+puts "thought I'd take a chance"
